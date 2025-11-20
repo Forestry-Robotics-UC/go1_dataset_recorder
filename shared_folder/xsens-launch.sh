118d739051519c90d5d22b3832c7116d6079e306
Duarte Cruz <duartecruz98@ik.me>
2025-11-20 18:26:30 +0000

    Add Dockerfiles entry-point scripts

---
+++ shared_folder/xsens-launch.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+#ROS 2 Middleware Implementation 
+
+#Fastrtps
+#export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+#export FASTDDS_BUILTIN_TRANSPORTS=UDPv4
+
+#Cyclonedds
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+
+# Source ROS2 Humble
+source /opt/ros/humble/setup.bash
+
+#Build workspace only with the packages descriminated on docker compose file
+cd /root/ros2_ws/
+colcon build --packages-select $BUILDLIST --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
+
+# Source ROS2 Workspace
+source /root/ros2_ws/install/setup.bash
+
+#Run xsens driver
+ros2 launch xsens_driver xsens_driver.launch.xml
